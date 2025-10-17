8b4594ba4e7d897fc542c35ea8c65d1801c5b500
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2025-10-17 08:07:45 +0530

    Integrate tRPC with React Query and add user query
    Added tRPC and @tanstack/react-query dependencies. Implemented tRPC server and client setup, including context, router, and query client utilities. Created a getUsers query in the tRPC router, updated the main page to use React Query hydration and a client component to fetch users, and wrapped the app in a TRPCReactProvider for context.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
-import prisma from "@/lib/db";
-import Image from "next/image";
+
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./client";
+import { Suspense } from "react";
 
 const Page = async () => {
-  const users = await prisma.user.findMany()
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions())
+
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
-        {JSON.stringify(users)}
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<p>loading...</p>}>
+          <Client />
+        </Suspense>
+      </HydrationBoundary>
     </div>
   );
-}
+};
 
 export default Page;
