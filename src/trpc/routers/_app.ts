1d68d2e18d8db7f843cc9cbe13f2af1967b775d9
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2025-10-24 11:47:59 +0530

    Integrate Inngest event-driven workflow
    Added Inngest client, functions, and API route to enable event-driven workflows. Updated dependencies in package.json and package-lock.json to include Inngest and related packages. Modified trpc router to support new event functionality.

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,10 +1,16 @@
+import { resolve } from "path";
 import { createTRPCRouter, protectedProcedure } from "../init";
 import prisma from "@/lib/db";
 export const appRouter = createTRPCRouter({
   getWorkflows: protectedProcedure.query(({ ctx }) => {
     return prisma.workflow.findMany();
   }),
-  createWorkflow: protectedProcedure.mutation(() => {
+  createWorkflow: protectedProcedure.mutation(async() => {
+
+    await new Promise((resolve) => setTimeout(resolve, 5_000))
+    await new Promise((resolve) => setTimeout(resolve, 5_000))
+    await new Promise((resolve) => setTimeout(resolve, 5_000))
+
     return prisma.workflow.create({
       data: {
         name: "test-workflow"
