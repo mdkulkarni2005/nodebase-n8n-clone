dc794e3429375be485be3d4d1cb2b07d711272ab
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2025-11-09 11:35:47 +0530

    Add Handlebars support for dynamic HTTP request endpoint and body

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -1,11 +1,20 @@
 import type { NodeExecutor } from "@/features/executions/types";
 import { NonRetriableError } from "inngest";
 import ky, { type Options as KyOption } from "ky";
+import Handlebars from "handlebars"
+import { resolve } from "path";
+
+Handlebars.registerHelper("json", (context) => {
+  const jsonString = JSON.stringify(context, null, 2)
+  const safeString =  new Handlebars.SafeString(jsonString)
+
+  return safeString
+})
 
 type HttpRequestData = {
-  variableName?: string;
-  endpoint?: string;
-  method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+  variableName: string;
+  endpoint: string;
+  method: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
   body: string;
 };
 
@@ -26,15 +35,22 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
     // TODO: Publish "error" state for http request
     throw new NonRetriableError("Variable name not configured");
   }
+  if (!data.method) {
+    // TODO: Publish "error" state for http request
+    throw new NonRetriableError("Method name not configured");
+  }
 
   const result = await step.run("http-request", async () => {
-    const endpoint = data.endpoint!;
-    const method = data.method || "GET";
+    const endpoint = Handlebars.compile(data.endpoint)(context);
+    console.log("ENDPOINT", {endpoint})
+    const method = data.method;
 
     const options: KyOption = { method };
 
     if (["POST", "PUT", "PATCH"].includes(method)) {
-      options.body = data.body;
+      const resolved = Handlebars.compile(data.body || "{}")(context)
+      JSON.parse(resolved)
+      options.body = resolved;
       options.headers = {
         "Content-Type": "application/json",
       };
@@ -53,17 +69,10 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
       },
     };
 
-    if (data.variableName) {
-      return {
-        ...context,
-        [data.variableName]: responsePayload,
-      };
-    }
-
     return {
       ...context,
-      ...responsePayload
-    }
+      [data.variableName]: responsePayload,
+    };
   });
 
   // TODO: Publish "success" state for http trigger
